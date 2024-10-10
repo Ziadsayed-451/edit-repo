80007422c057f1b6ca164d5234fddd2c50837bbb
Ziad <zsyd23533@gmail.com>
2024-10-10 19:23:09 +0300

    First commit

---
+++ MyForm.h
@@ -0,0 +1,195 @@
+﻿#pragma once
+
+namespace GuiApp {
+
+	using namespace System;
+	using namespace System::ComponentModel;
+	using namespace System::Collections;
+	using namespace System::Windows::Forms;
+	using namespace System::Data;
+	using namespace System::Drawing;
+
+	/// <summary>
+	/// Summary for MyForm
+	/// </summary>
+	public ref class MyForm : public System::Windows::Forms::Form
+	{
+	public:
+		MyForm(void)
+		{
+			InitializeComponent();
+			//
+			//TODO: Add the constructor code here
+			//
+		}
+
+	protected:
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		~MyForm()
+		{
+			if (components)
+			{
+				delete components;
+			}
+		}
+	private: System::Windows::Forms::Panel^ status_bar;
+	private: System::Windows::Forms::Panel^ panel1;
+	private: System::Windows::Forms::Panel^ panel2;
+	private: System::Windows::Forms::Panel^ panel3;
+	private: System::Windows::Forms::TextBox^ textBox1;
+
+
+	protected:
+
+	protected:
+
+	protected:
+
+	protected:
+
+	protected:
+
+	protected:
+
+
+
+
+
+
+
+
+
+
+
+
+	protected:
+
+
+
+
+	protected:
+
+	protected:
+
+	protected:
+
+	protected:
+	private: System::ComponentModel::IContainer^ components;
+
+
+	protected:
+
+	private:
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+
+
+#pragma region Windows Form Designer generated code
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		void InitializeComponent(void)
+		{
+			this->status_bar = (gcnew System::Windows::Forms::Panel());
+			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
+			this->panel1 = (gcnew System::Windows::Forms::Panel());
+			this->panel2 = (gcnew System::Windows::Forms::Panel());
+			this->panel3 = (gcnew System::Windows::Forms::Panel());
+			this->status_bar->SuspendLayout();
+			this->SuspendLayout();
+			// 
+			// status_bar
+			// 
+			this->status_bar->Controls->Add(this->textBox1);
+			this->status_bar->Dock = System::Windows::Forms::DockStyle::Top;
+			this->status_bar->Location = System::Drawing::Point(0, 0);
+			this->status_bar->Margin = System::Windows::Forms::Padding(6, 5, 6, 5);
+			this->status_bar->Name = L"status_bar";
+			this->status_bar->Size = System::Drawing::Size(706, 27);
+			this->status_bar->TabIndex = 0;
+			// 
+			// textBox1
+			// 
+			this->textBox1->BackColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(64)), static_cast<System::Int32>(static_cast<System::Byte>(64)),
+				static_cast<System::Int32>(static_cast<System::Byte>(64)));
+			this->textBox1->BorderStyle = System::Windows::Forms::BorderStyle::None;
+			this->textBox1->Font = (gcnew System::Drawing::Font(L"Microsoft Tai Le", 11.25F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->textBox1->ForeColor = System::Drawing::Color::White;
+			this->textBox1->Location = System::Drawing::Point(569, 3);
+			this->textBox1->Name = L"textBox1";
+			this->textBox1->Size = System::Drawing::Size(134, 20);
+			this->textBox1->TabIndex = 0;
+			this->textBox1->Text = L"مرحبا, زياد سيد  [خروج]";
+			// 
+			// panel1
+			// 
+			this->panel1->Dock = System::Windows::Forms::DockStyle::Top;
+			this->panel1->Location = System::Drawing::Point(0, 27);
+			this->panel1->Name = L"panel1";
+			this->panel1->Size = System::Drawing::Size(706, 38);
+			this->panel1->TabIndex = 1;
+			this->panel1->Paint += gcnew System::Windows::Forms::PaintEventHandler(this, &MyForm::panel1_Paint);
+			// 
+			// panel2
+			// 
+			this->panel2->Dock = System::Windows::Forms::DockStyle::Right;
+			this->panel2->Location = System::Drawing::Point(506, 65);
+			this->panel2->Name = L"panel2";
+			this->panel2->Size = System::Drawing::Size(200, 449);
+			this->panel2->TabIndex = 2;
+			// 
+			// panel3
+			// 
+			this->panel3->Dock = System::Windows::Forms::DockStyle::Fill;
+			this->panel3->Font = (gcnew System::Drawing::Font(L"Microsoft Tai Le", 11.25F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->panel3->Location = System::Drawing::Point(0, 65);
+			this->panel3->Name = L"panel3";
+			this->panel3->Size = System::Drawing::Size(506, 449);
+			this->panel3->TabIndex = 3;
+			// 
+			// MyForm
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(11, 23);
+			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->BackColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(45)), static_cast<System::Int32>(static_cast<System::Byte>(45)),
+				static_cast<System::Int32>(static_cast<System::Byte>(45)));
+			this->ClientSize = System::Drawing::Size(706, 514);
+			this->Controls->Add(this->panel3);
+			this->Controls->Add(this->panel2);
+			this->Controls->Add(this->panel1);
+			this->Controls->Add(this->status_bar);
+			this->Cursor = System::Windows::Forms::Cursors::Arrow;
+			this->Font = (gcnew System::Drawing::Font(L"Microsoft Tai Le", 14.25F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->ForeColor = System::Drawing::Color::White;
+			this->HelpButton = true;
+			this->Margin = System::Windows::Forms::Padding(6, 5, 6, 5);
+			this->Name = L"MyForm";
+			this->Text = L"Hello To My First GUI";
+			this->TransparencyKey = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(255)), static_cast<System::Int32>(static_cast<System::Byte>(128)),
+				static_cast<System::Int32>(static_cast<System::Byte>(0)));
+			this->Load += gcnew System::EventHandler(this, &MyForm::MyForm_Load);
+			this->status_bar->ResumeLayout(false);
+			this->status_bar->PerformLayout();
+			this->ResumeLayout(false);
+
+		}
+#pragma endregion
+	private: System::Void MyForm_Load(System::Object^ sender, System::EventArgs^ e) {
+	}
+	private: System::Void textBox1_TextChanged(System::Object^ sender, System::EventArgs^ e) {
+	}
+	private: System::Void bindingNavigatorAddNewItem_Click(System::Object^ sender, System::EventArgs^ e) {
+	}
+private: System::Void textBox1_TextChanged_1(System::Object^ sender, System::EventArgs^ e) {
+}
+private: System::Void panel1_Paint(System::Object^ sender, System::Windows::Forms::PaintEventArgs^ e) {
+}
+};
+}
